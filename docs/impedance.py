3c7be44120a923fa84497e24fc896315b951a77b
/C=US/ST=Massachusetts/L=Cambridge/O=Wilson Lab, PCLM, MIT/OU=The Soma Project/CN=jonas/emailAddress=jonas@soma.mit.edu </C=US/ST=Massachusetts/L=Cambridge/O=Wilson Lab, PCLM, MIT/OU=The Soma Project/CN=jonas/emailAddress=jonas@soma.mit.edu@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2006-03-03 01:25:33 +0000

    * Devicelink model for how we communicate with peripherals.
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/backplane/trunk@1316 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- docs/impedance.py
+++ docs/impedance.py
@@ -1,14 +1,22 @@
 
 from scipy import *
-epsilon = 4.6
-w = 12 # trace width 
-s = 24 # trace separation 
+
+def calcZ(w, s, t, h, epsilon=4.6) :
+    Z0 = 60. / sqrt(0.457*epsilon +0.67)*log(4*h/(0.67*(0.8*w + t)))
+
+    Zdiff = 2*Z0 * (1- 0.48*exp(-0.96*(s/h)))
+    
+
+    return (Z0, Zdiff)
+
+w = 11 # trace width 
+s = 14 # trace separation 
 t = 1.4 # trace thickness 
 h = 7.5 # height above ground plane
 
-Z0 = 60. / sqrt(0.457*epsilon +0.67)*log(4*h/(0.67*(0.8*w + t)))
+print calcZ(w, s, t, h)
 
-Zdiff = 2*Z0 * (1- 0.48*exp(-0.96*(s/h)))
+lw = w*2+s+1.5*s
+print "Lane width = ", lw, " mil" 
 
-print "Z0 = ", Z0
-print "Zdiff = ", Zdiff
+print "total width = " ,  lw*12
