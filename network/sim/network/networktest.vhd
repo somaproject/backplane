1200f2c85ce2a5a05ad4f6df72739e8bfefcc000
/C=US/ST=Massachusetts/L=Cambridge/O=soma/CN=Eric Jonas/emailAddress=jonas@soma.mit.edu </C=US/ST=Massachusetts/L=Cambridge/O=soma/CN=Eric Jonas/emailAddress=jonas@soma.mit.edu@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2006-07-12 21:01:20 +0000

    initial network sim
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/backplane/trunk@1578 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

---
+++ network/sim/network/networktest.vhd
@@ -0,0 +1,23 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.STD_LOGIC_ARITH.all;
+use IEEE.STD_LOGIC_UNSIGNED.all;
+
+library UNISIM;
+use UNISIM.vcomponents.all;
+
+library WORK;
+use WORK.networkstack;
+
+entity networktest is
+  
+end networktest;
+
+architecture Behavioral of networktest is
+
+begin  -- Behavioral
+
+  
+
+end Behavioral;
