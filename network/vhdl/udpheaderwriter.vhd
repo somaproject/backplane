441f3f2db10d132b8ada674b19d7d5f256c97943
/C=US/ST=Massachusetts/L=Cambridge/O=soma/CN=Eric Jonas/emailAddress=jonas@soma.mit.edu </C=US/ST=Massachusetts/L=Cambridge/O=soma/CN=Eric Jonas/emailAddress=jonas@soma.mit.edu@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2006-07-19 13:13:14 +0000

    * migration of event header writer towards a more generic role in udp construction.
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/backplane/trunk@1594 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- network/vhdl/udpheaderwriter.vhd
+++ network/vhdl/udpheaderwriter.vhd
@@ -7,24 +7,25 @@ library WORK;
 use WORK.somabackplane.all;
 use work.somabackplane;
 
-entity eventheaderwriter is
+entity udpheaderwriter is
   port (
-    CLK   : in  std_logic;
-    MYMAC : in  std_logic_vector(47 downto 0);
-    MYIP  : in  std_logic_vector(31 downto 0);
-    MYBCAST : in std_logic_vector(31 downto 0); 
-    START : in  std_logic;
-    WLEN  : in  std_logic_vector(9 downto 0);
-    DOUT  : out std_logic_vector(15 downto 0);
-    WEOUT : out std_logic;
-    ADDR  : out std_logic_vector(9 downto 0);
-    DONE  : out std_logic);
-end eventheaderwriter;
+    CLK      : in  std_logic;
+    MYMAC    : in  std_logic_vector(47 downto 0);
+    MYIP     : in  std_logic_vector(31 downto 0);
+    MYBCAST  : in  std_logic_vector(31 downto 0);
+    DESTPORT : in  std_logic_vector(15 downto 0);
+    START    : in  std_logic;
+    WLEN     : in  std_logic_vector(9 downto 0);
+    DOUT     : out std_logic_vector(15 downto 0);
+    WEOUT    : out std_logic;
+    ADDR     : out std_logic_vector(9 downto 0);
+    DONE     : out std_logic);
+end udpheaderwriter;
 
 
-architecture Behavioral of eventheaderwriter is
+architecture Behavioral of udpheaderwriter is
 
-  signal dmux                    : integer range 0 to 10          := 0;
+  signal dmux                    : integer range 0 to 11         := 0;
   signal udplen, iplen, framelen : std_logic_vector(15 downto 0) := (others => '0');
 
   signal cdin : std_logic_vector(15 downto 0) := (others => '0');
@@ -37,9 +38,9 @@ architecture Behavioral of eventheaderwriter is
 
   signal doutint : std_logic_vector(15 downto 0) := (others => '0');
 
-  
+
   type states is (none, macwh, macwm, macwl, ipwh, ipwl, bcastwh, bcastwl,
-                  udplenw, iplenw, framelw, chksumw);
+                  udpportw,             udplenw, iplenw, framelw, chksumw);
 
   signal cs, ns : states := none;
 
@@ -54,7 +55,7 @@ architecture Behavioral of eventheaderwriter is
 
 begin  -- Behavioral
 
-    ipchecksum_inst : ipchecksum
+  ipchecksum_inst : ipchecksum
     port map (
       CLK    => CLK,
       DIN    => cdin,
@@ -63,27 +64,28 @@ begin  -- Behavioral
       CHKOUT => csum);
 
   ain(10 downto 1) <= WLEN;
-  udplen          <= ain + X"0008";
-  iplen           <= ain + X"001c";
-  framelen        <= ain + X"002c";
+  udplen           <= ain + X"0008";
+  iplen            <= ain + X"001c";
+  framelen         <= ain + X"002c";
 
-  doutint <= MYMAC(47 downto 32) when dmux = 0 else
-             MYMAC(31 downto 16) when dmux = 1 else
-             MYMAC(15 downto 0)  when dmux = 2 else
-             MYIP(31 downto 16)  when dmux = 3 else
-             MYIP(15 downto 0)   when dmux = 4 else
-             MYBCAST(31 downto 16)  when dmux = 5 else
-             MYBCAST(15 downto 0)   when dmux = 6 else
-             udplen              when dmux = 7 else
-             iplen               when dmux = 8 else
-             framelen            when dmux = 9 else
+  doutint <= MYMAC(47 downto 32)   when dmux = 0  else
+             MYMAC(31 downto 16)   when dmux = 1  else
+             MYMAC(15 downto 0)    when dmux = 2  else
+             MYIP(31 downto 16)    when dmux = 3  else
+             MYIP(15 downto 0)     when dmux = 4  else
+             MYBCAST(31 downto 16) when dmux = 5  else
+             MYBCAST(15 downto 0)  when dmux = 6  else
+             DESTPORT              when dmux = 7  else
+             udplen                when dmux = 8  else
+             iplen                 when dmux = 9  else
+             framelen              when dmux = 10 else
              csum;
   DOUT    <= doutint;
-  cdin    <= doutint             when ld = '0' else X"8511";
+  cdin    <= doutint               when ld = '0'  else X"8511";
 
   ld <= '1' when cs = none else '0';
 
-  DONE <= '1' when cs = chksumw else '0'; 
+  DONE   <= '1' when cs = chksumw else '0';
   main : process(CLK)
   begin
     if rising_edge(CLK) then
@@ -139,52 +141,59 @@ begin  -- Behavioral
         addr  <= "0000001111";
         chken <= '1';
         weout <= '1';
-        ns    <= bcastwh; 
+        ns    <= bcastwh;
 
       when bcastwh =>
         dmux  <= 5;
         addr  <= "0000010000";
         chken <= '1';
         weout <= '1';
-        ns    <= bcastwl; 
+        ns    <= bcastwl;
 
       when bcastwl =>
         dmux  <= 6;
         addr  <= "0000010001";
         chken <= '1';
         weout <= '1';
+        ns    <= udpportw;
+
+      when udpportw =>
+        dmux  <= 7;
+        addr  <= "0000010011";
+        chken <= '0';
+        weout <= '1';
         ns    <= udplenw;
 
       when udplenw =>
-        dmux  <= 7;
+        dmux  <= 8;
         addr  <= "0000010100";
         chken <= '0';
         weout <= '1';
         ns    <= iplenw;
 
       when iplenw =>
-        dmux  <= 8;
+        dmux  <= 9;
         addr  <= "0000001001";
         chken <= '1';
         weout <= '1';
         ns    <= framelw;
 
       when framelw =>
-        dmux  <= 9;
+        dmux  <= 10;
         addr  <= "0000000000";
         chken <= '0';
         weout <= '1';
         ns    <= chksumw;
 
       when chksumw =>
-        dmux  <= 10;
+        dmux  <= 11;
         addr  <= "0000001101";
         chken <= '0';
         weout <= '1';
         ns    <= none;
 
       when others =>
-        dmux  <= 10;
+        dmux  <= 11;
         addr  <= "0000001101";
         chken <= '0';
         weout <= '0';
