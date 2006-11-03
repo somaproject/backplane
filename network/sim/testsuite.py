0d4bbdc4e7cb41c5d1e7f171c31cc4c5cfada936
/C=US/ST=Massachusetts/L=Cambridge/O=soma/CN=Eric Jonas/emailAddress=jonas@soma.mit.edu </C=US/ST=Massachusetts/L=Cambridge/O=soma/CN=Eric Jonas/emailAddress=jonas@soma.mit.edu@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2006-11-03 01:21:33 +0000

    * test suite aggregator
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/backplane/trunk@1826 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

---
+++ network/sim/testsuite.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import vhdltest
+import unittest
+import sys
+
+
+suite = unittest.TestSuite()
+
+vhdlTestCase = vhdltest.SymphonyVhdlSimTestCase
+
+if len(sys.argv) > 1 :
+    # run those from the command line
+    for i in sys.argv[1:]:
+        suite.addTest(vhdlTestCase(i))
+        
+else:
+
+    # core components
+    
+    suite.addTest(vhdlTestCase("txmux"))
+    
+runner = unittest.TextTestRunner()
+runner.run(suite)
