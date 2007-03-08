15e64201f07e80dccc8049f4d0846502c9f9265c
/C=US/ST=Massachusetts/L=Cambridge/O=soma/CN=Eric Jonas/emailAddress=jonas@soma.mit.edu </C=US/ST=Massachusetts/L=Cambridge/O=soma/CN=Eric Jonas/emailAddress=jonas@soma.mit.edu@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2007-03-08 00:10:18 +0000

    * updated nettest, not synthing on time :(
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/backplane/trunk@2080 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- tests/nettest/sim/nettesttest.vhd
+++ tests/nettest/sim/nettesttest.vhd
@@ -17,26 +17,44 @@ architecture Behavioral of nettesttest is
 
   component nettest
     port (
-      CLKIN        : in  std_logic;
-      SERIALBOOT : out std_logic_vector(19 downto 0);
-      SDOUT      : out std_logic;
-      SDIN       : in  std_logic;
-      SCLK       : out std_logic;
-      SCS        : out std_logic;
-      NICFCLK    : out std_logic;
-      NICFDIN    : out std_logic;
-      NICFPROG   : out std_logic;
-    NETDOUT : out std_logic_vector(15 downto 0);
-    NETNEWFRAME : out std_logic;
-    NETCLK : out std_logic;
+      CLKIN        : in    std_logic;
+      SERIALBOOT   : out   std_logic_vector(19 downto 0);
+      SDOUT        : out   std_logic;
+      SDIN         : in    std_logic;
+      SCLK         : out   std_logic;
+      SCS          : out   std_logic;
+      LEDPOWER     : out   std_logic;
+      LEDEVENT     : out   std_logic;
+      NICFCLK      : out   std_logic;
+      NICFDIN      : out   std_logic;
+      NICFPROG     : out   std_logic;
+      NICSCLK      : out   std_logic;
+      NICSIN       : in    std_logic;
+      NICSOUT      : out   std_logic;
+      NICSCS       : out   std_logic;
+      NICDOUT      : out   std_logic_vector(15 downto 0);
+      NICNEWFRAME  : out   std_logic;
+      NICDIN       : in    std_logic_vector(15 downto 0);
+      NICNEXTFRAME : out   std_logic;
+      NICDINEN     : in    std_logic;
+      NICIOCLK     : out   std_logic;
+      RAMCLKOUT_P  : out   std_logic;
+      RAMCLKOUT_N  : out   std_logic;
+      RAMCKE       : out   std_logic := '0';
+      RAMCAS       : out   std_logic;
+      RAMRAS       : out   std_logic;
+      RAMCS        : out   std_logic;
+      RAMWE        : out   std_logic;
+      RAMADDR      : out   std_logic_vector(12 downto 0);
+      RAMBA        : out   std_logic_vector(1 downto 0);
+      RAMDQSH      : inout std_logic;
+      RAMDQSL      : inout std_logic;
+      RAMDQ        : inout std_logic_vector(15 downto 0)
 
-      LEDPOWER : out std_logic;
-      LEDEVENT : out std_logic
       );
-
   end component;
 
-  signal CLKIN, CLK        : std_logic                     := '0';
+  signal CLKIN, CLK : std_logic                     := '0';
   signal SERIALBOOT : std_logic_vector(19 downto 0) := (others => '0');
 
   signal SDOUT    : std_logic := '0';
@@ -49,9 +67,29 @@ architecture Behavioral of nettesttest is
   signal NICFCLK  : std_logic := '0';
   signal NICFDIN  : std_logic := '0';
   signal NICFPROG : std_logic := '0';
-  signal NETDOUT :  std_logic_vector(15 downto 0);
-  signal NETNEWFRAME : std_logic;
-  signal NETCLK : std_logic;
+
+  signal NICSCLK      : std_logic                     := '0';
+  signal NICSIN       : std_logic                     := '0';
+  signal NICSOUT      : std_logic                     := '0';
+  signal NICSCS       : std_logic                     := '0';
+  signal NICDOUT      : std_logic_vector(15 downto 0) := (others => '0');
+  signal NICNEWFRAME  : std_logic                     := '0';
+  signal NICDIN       : std_logic_vector(15 downto 0) := (others => '0');
+  signal NICNEXTFRAME : std_logic                     := '0';
+  signal NICDINEN     : std_logic                     := '0';
+  signal NICIOCLK     : std_logic                     := '0';
+  signal RAMCLKOUT_P  : std_logic                     := '0';
+  signal RAMCLKOUT_N  : std_logic                     := '0';
+  signal RAMCKE       : std_logic                     := '0';
+  signal RAMCAS       : std_logic                     := '0';
+  signal RAMRAS       : std_logic                     := '0';
+  signal RAMCS        : std_logic                     := '0';
+  signal RAMWE        : std_logic                     := '0';
+  signal RAMADDR      : std_logic_vector(12 downto 0) := (others => '0');
+  signal RAMBA        : std_logic_vector(1 downto 0)  := (others => '0');
+  signal RAMDQSH      : std_logic                     := '0';
+  signal RAMDQSL      : std_logic                     := '0';
+  signal RAMDQ        : std_logic_vector(15 downto 0) := (others => '0');
 
   component mmc
     generic (
@@ -64,54 +102,78 @@ architecture Behavioral of nettesttest is
       SCS   : in  std_logic );
   end component;
 
---   component bootdeserialize
---     port (
---       CLK   : in  std_logic;
---       DIN   : in  std_logic;
---       FPROG : out std_logic;
---       FDIN  : out std_logic;
---       FCLK  : out std_logic);
---   end component;
+-- component bootdeserialize
+-- port (
+-- CLK : in std_logic;
+-- DIN : in std_logic;
+-- FPROG : out std_logic;
+-- FDIN : out std_logic;
+-- FCLK : out std_logic);
+-- end component;
 
-  
-component simplefpga
 
-  port (
-    START     : in  std_logic;
-    BOOTADDR  : in  std_logic_vector(15 downto 0);
-    BOOTLEN   : in  std_logic_vector(15 downto 0);
-    FCLK      : in  std_logic;
-    FDIN      : in  std_logic;
-    FPROG     : in  std_logic;
-    VALIDBOOT : out std_logic
-    );
+  component simplefpga
 
-end component;
+    port (
+      START     : in  std_logic;
+      BOOTADDR  : in  std_logic_vector(15 downto 0);
+      BOOTLEN   : in  std_logic_vector(15 downto 0);
+      FCLK      : in  std_logic;
+      FDIN      : in  std_logic;
+      FPROG     : in  std_logic;
+      VALIDBOOT : out std_logic
+      );
 
-signal nicfpgastart, nicvalidboot : std_logic := '0';
+  end component;
 
-  
+  signal nicfpgastart, nicvalidboot : std_logic := '0';
+
+
 begin  -- Behavioral
 
   nettest_uut : nettest
     port map (
-      CLKIN        => CLKIN,
+      CLKIN      => CLKIN,
       SERIALBOOT => SERIALBOOT,
       SDOUT      => SDOUT,
       SDIN       => SDIN,
       SCLK       => SCLK,
       SCS        => SCS,
+      LEDPOWER   => LEDPOWER,
+      LEDEVENT   => LEDEVENT,
       NICFCLK    => NICFCLK,
       NICFDIN    => NICFDIN,
       NICFPROG   => NICFPROG,
-      LEDPOWER   => LEDPOWER,
-      LEDEVENT   => LEDEVENT,
-      NETDOUT => NETDOUT,
-      NETNEWFRAME => NETNEWFRAME,
-      NETCLK => NETCLK);
+      -- NIC SERIAL INTERFACE
+      NICSCLK    => NICSCLK,
+      NICSIN     => NICSIN,
+      NICSOUT    => NICSOUT,
+      NICSCS     => NICSCS,
+      -- NIC DATA INTERFACE
 
-  CLKIN <= not CLKIN after 16.666666  ns;
-  CLK <= CLKIN; 
+      NICDOUT      => NICDOUT,
+      NICNEWFRAME  => NICNEWFRAME,
+      NICDIN       => NICDIN,
+      NICNEXTFRAME => NICNEXTFRAME,
+      NICDINEN     => NICDINEN,
+      NICIOCLK     => NICIOCLK,
+      -- RAM INTERFACE
+      RAMCLKOUT_P  => RAMCLKOUT_P,
+      RAMCLKOUT_N  => RAMCLKOUT_N,
+      RAMCKE       => RAMCKE,
+      RAMCAS       => RAMCAS,
+      RAMRAS       => RAMRAS,
+      RAMCS        => RAMCS,
+      RAMWE        => RAMWE,
+      RAMADDR      => RAMADDR,
+      RAMBA        => RAMBA,
+      RAMDQSH      => RAMDQSH,
+      RAMDQSL      => RAMDQSL,
+      RAMDQ        => RAMDQ);
+
+
+  CLKIN <= not CLKIN after 10 ns; 
+  CLK   <= CLKIN;
 
   mmc_inst : mmc
     generic map (
@@ -123,7 +185,7 @@ begin  -- Behavioral
       SDOUT => SDIN,
       SCS   => SCS);
 
-  nicfpga: simplefpga
+  nicfpga : simplefpga
     port map (
       START     => nicfpgastart,
       bootaddr  => X"2000",
@@ -133,17 +195,17 @@ begin  -- Behavioral
       FPROG     => NICFPROG,
       VALIDBOOT => nicvalidboot);
 
-  main: process
-    begin
-      nicfpgastart <= '1';
-      wait for 100 ns;
-      nicfpgastart <= '0';
-      wait until rising_edge(CLK) and nicvalidboot = '1';
-      assert False report "End of Simulation" severity failure;
+  main : process
+  begin
+    nicfpgastart <= '1';
+    wait for 100 ns;
+    nicfpgastart <= '0';
+    wait until rising_edge(CLK) and nicvalidboot = '1';
+    assert false report "End of Simulation" severity failure;
 
-      
-      wait;
-      
-    end process; 
-    
+
+    wait;
+
+  end process;
+
 end Behavioral;
