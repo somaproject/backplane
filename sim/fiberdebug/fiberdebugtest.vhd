f469208a99f4c44111831ae59250872df1ca84b4
/C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Eric M Jonas/emailAddress=jonas@MIT.EDU </C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Eric M Jonas/emailAddress=jonas@MIT.EDU@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2007-07-24 11:24:17 +0000

    * created coregen xcos and readme
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/backplane/trunk@2310 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- sim/fiberdebug/fiberdebugtest.vhd
+++ sim/fiberdebug/fiberdebugtest.vhd
@@ -9,204 +9,84 @@ library WORK;
 use WORK.somabackplane.all;
 use work.somabackplane;
 
-entity rxeventfifotest is
+entity fiberdebugtest is
 
-end rxeventfifotest;
+end fiberdebugtest;
 
-architecture Behavioral of rxeventfifotest is
+architecture Behavioral of fiberdebugtest is
 
-  component rxeventfifo
-    port (
-      CLK    : in  std_logic;
-      RESET  : in  std_logic;
-      ECYCLE : in  std_logic;
-      EATX   : in  std_logic_vector(somabackplane.N -1 downto 0);
-      EDTX   : in  std_logic_vector(7 downto 0);
-      -- outputs
-      EOUTD  : out std_logic_vector(15 downto 0);
-      EOUTA  : in  std_logic_vector(2 downto 0);
-      EVALID : out std_logic;
-      ENEXT  : in  std_logic
+  component fiberdebug
+    generic (
+      DEVICE  :     std_logic_vector(7 downto 0) := X"01"
       );
+    port (
+      CLK     : in  std_logic;
+      TXCLK   : in  std_logic;
+      RESET   : in  std_logic;
+      -- Event bus interface
+      ECYCLE  : in  std_logic;
+      EARXA   : out std_logic_vector(somabackplane.N - 1 downto 0)
+                                                 := (others => '0');
+      EDRXA   : out std_logic_vector(7 downto 0);
+      EARXB   : out std_logic_vector(somabackplane.N - 1 downto 0)
+                                                 := (others => '0');
+      EDRXB   : out std_logic_vector(7 downto 0);
+      EDSELRX : in  std_logic_vector(3 downto 0);
+      EATX    : in  std_logic_vector(somabackplane.N - 1 downto 0);
+      EDTX    : in  std_logic_vector(7 downto 0);
+
+      -- Fiber interfaces
+      FIBERIN  : in  std_logic;
+      FIBEROUT : out std_logic
+      );
+
   end component;
 
 
-  signal CLK    : std_logic := '0';
-  signal RESET  : std_logic := '1';
-  signal ECYCLE : std_logic := '0';
+  signal CLK     : std_logic                    := '0';
+  signal TXCLK   : std_logic                    := '0';
+  signal RESET   : std_logic                    := '1';
+  -- Event bus interface
+  signal ECYCLE  : std_logic                    := '0';
+  signal EARXA   : std_logic_vector(somabackplane.N - 1 downto 0)
+                                                := (others => '0');
+  signal EDRXA   : std_logic_vector(7 downto 0) := (others => '0');
+  signal EARXB   : std_logic_vector(somabackplane.N - 1 downto 0)
+                                                := (others => '0');
+  signal EDRXB   : std_logic_vector(7 downto 0) := (others => '0');
+  signal EDSELRX : std_logic_vector(3 downto 0) := (others => '0');
+  signal EATX    : std_logic_vector(somabackplane.N - 1 downto 0)
+                                                := (others => '0');
+  signal EDTX    : std_logic_vector(7 downto 0) := (others => '0');
 
-  signal EATX : std_logic_vector(somabackplane.N -1 downto 0) := (others => '0');
+  -- Fiber interfaces
+  signal FIBERIN  : std_logic := '0';
+  signal FIBEROUT : std_logic := '0';
 
-  signal EDTX   : std_logic_vector(7 downto 0)  := (others => '0');
-  signal EOUTD  : std_logic_vector(15 downto 0) := (others => '0');
-  signal EOUTA  : std_logic_vector(2 downto 0)  := (others => '0');
-  signal EVALID : std_logic                     := '0';
-  signal ENEXT  : std_logic                     := '0';
-
-  signal pos : integer := 990;
-
-  signal expectedeventid : std_logic_vector(7 downto 0) := (others => '0');
 
 begin  -- Behavioral
 
-  rxeventfifo_uut : rxeventfifo
+  CLK   <= not CLK   after 10 ns;
+  TXCLK <= not TXCLK after 6.25 ns;
+
+  RESET <= '0' after 100 ns;
+
+  fiberdebug_uut: fiberdebug
+    generic map (
+      DEVICE => X"01")
     port map (
-      CLK    => CLK,
-      RESET  => RESET,
-      ECYCLE => ECYCLE,
-      EATX   => EATX,
-      EDTX   => EDTX,
-      EOUTD  => EOUTD,
-      EOUTA  => EOUTA,
-      EVALID => EVALID,
-      ENEXT  => ENEXT);
-
-
-  CLK <= not CLK after 10 ns;
-
-  RESET <= '0' after 50 ns;
-
-
-  -- input generation
-
-  ecycle_generation : process(CLK)
-  begin
-    if rising_edge(CLK) then
-      if pos = 999 then
-        pos <= 0;
-      else
-        pos <= pos + 1;
-      end if;
-
-      if pos = 999 then
-        ECYCLE <= '1' after 4 ns;
-      else
-        ECYCLE <= '0' after 4 ns;
-      end if;
-    end if;
-  end process ecycle_generation;
-
-
-  event_packet_generation : process
-  begin
-
-    while true loop
-
-      wait until rising_edge(CLK) and pos = 47;
-      -- now we send the events
-      for i in 0 to somabackplane.N -1 loop
-        EDTX <= std_logic_vector(TO_UNSIGNED(i, 8));
-        wait until rising_edge(CLK);
-        EDTX <= X"00";
-        wait until rising_edge(CLK);
-
-        EDTX <= std_logic_vector(TO_UNSIGNED(i, 8));
-        wait until rising_edge(CLK);
-        EDTX <= X"01";
-        wait until rising_edge(CLK);
-
-        EDTX <= std_logic_vector(TO_UNSIGNED(i, 8));
-        wait until rising_edge(CLK);
-        EDTX <= X"02";
-        wait until rising_edge(CLK);
-
-        EDTX <= std_logic_vector(TO_UNSIGNED(i, 8));
-        wait until rising_edge(CLK);
-        EDTX <= X"03";
-        wait until rising_edge(CLK);
-
-        EDTX <= std_logic_vector(TO_UNSIGNED(i, 8));
-        wait until rising_edge(CLK);
-        EDTX <= X"04";
-        wait until rising_edge(CLK);
-
-        EDTX <= std_logic_vector(TO_UNSIGNED(i, 8));
-        wait until rising_edge(CLK);
-        EDTX <= X"05";
-        wait until rising_edge(CLK);
-
-      end loop;  -- i
-    end loop;
-
-  end process;
-
-  event_transmission : process
-  begin
-    wait until rising_edge(CLK) and ECYCLE = '1';
-    EATX <= (others => '0');
-
-
-    wait until rising_edge(CLK) and ECYCLE = '1';
-
-    assert EVALID = '0'
-      report "Fifo incorrectly acquired an event during an empty (EATX = 0) cycle"
-      severity error;
-
-
-    EATX(0) <= '1';
-    wait until rising_edge(CLK) and EVALID = '1';
-    -- verify event
-    EOUTA   <= "000";
-
-    expectedeventid <= X"00";
-    for i in 0 to 5 loop
-      EOUTA         <= std_logic_vector(TO_UNSIGNED(i, 3)) after 1 ns;
-      wait until rising_edge(CLK);
-      wait for 1 ns;
-
-      assert EOUTD = expectedeventid & std_logic_vector(TO_UNSIGNED(i, 8))
-        report "Error reading data" severity error;
-
-
-    end loop;  -- i
-    wait until rising_edge(CLK);
-    ENEXT <= '1' after 3 ns;
-    wait until rising_edge(CLK);
-    ENEXT <= '0' after 3 ns;
-
-    -- a bunch of events
-    wait until rising_edge(CLK) and ECYCLE = '1';
-
-    -- add 22 events
-    EATX(19 downto 0) <= (others => '1');
-    --EATX(23 downto 22) <= (others => '1');
-    wait until rising_edge(CLK) and ECYCLE = '1';
-
-    EATX <= (others => '0');
+      CLK      => CLK,
+      TXCLK    => TXCLK,
+      RESET    => RESET,
+      ECYCLE   => ECYCLE,
+      EARXA    => EARXA,
+      EDRXA    => EDRXA,
+      EARXB    => EARXB,
+      EDRXB    => EDRXB,
+      EDSELRX  => EDSELRX,
+      EATX     => EATX,
+      EDTX     => EDTX,
+      FIBERIN  => FIBERIN,
+      FIBEROUT => FIBEROUT);
     
-    wait until rising_edge(CLK) and EVALID = '1';
-    -- verify event
-    EOUTA <= "000";
-
-    expectedeventid <= X"00";
-    for eventid in 0 to 19 loop
-      wait until rising_edge(CLK) and EVALID = '1';
-      for i in 0 to 5 loop
-        EOUTA       <= std_logic_vector(TO_UNSIGNED(i, 3)) after 1 ns;
-        wait until rising_edge(CLK);
-        wait for 1 ns;
-
-        assert EOUTD = std_logic_vector(TO_UNSIGNED(eventid, 8))
-          & std_logic_vector(TO_UNSIGNED(i, 8))
-          report "Error reading data" severity error;
-
-      end loop;
-
-      wait until rising_edge(CLK);
-      ENEXT <= '1' after 3 ns;
-      wait until rising_edge(CLK);
-      ENEXT <= '0' after 3 ns;
-
-
-
-    end loop;  -- eventid
-
-    wait for 20 us;
-    assert false report "End of Simulation" severity failure;
-
-
-    wait;
-
-  end process event_transmission;
-
 end Behavioral;
