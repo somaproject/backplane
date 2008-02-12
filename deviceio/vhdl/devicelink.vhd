b71ba28f7ec62ed610aa22dd755aa6f38b52fe04
/C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Eric M Jonas/emailAddress=jonas@MIT.EDU </C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Eric M Jonas/emailAddress=jonas@MIT.EDU@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2008-02-12 04:12:42 +0000

    * in a very daring move, we added two BUFGs to the devicelink rxhibit clocks to make routing easier, at the expense of burning some of our precious spartan-3 global clock buffers.
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/backplane/trunk@2614 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- deviceio/vhdl/devicelink.vhd
+++ deviceio/vhdl/devicelink.vhd
@@ -10,36 +10,36 @@ use UNISIM.VComponents.all;
 
 entity devicelink is
   port (
-    TXCLKIN     : in  std_logic;
-    TXLOCKED    : in  std_logic;
-    TXDIN       : in  std_logic_vector(9 downto 0);
-    TXDOUT      : out std_logic_vector(7 downto 0);
-    TXKOUT      : out std_logic;
-    CLK         : out std_logic;
-    CLK2X       : out std_logic;
-    RESET       : out std_logic;
-    RXDIN       : in  std_logic_vector(7 downto 0);
-    RXKIN       : in  std_logic;
-    RXIO_P      : out std_logic;
-    RXIO_N      : out std_logic;
+    TXCLKIN    : in  std_logic;
+    TXLOCKED   : in  std_logic;
+    TXDIN      : in  std_logic_vector(9 downto 0);
+    TXDOUT     : out std_logic_vector(7 downto 0);
+    TXKOUT     : out std_logic;
+    CLK        : out std_logic;
+    CLK2X      : out std_logic;
+    RESET      : out std_logic;
+    RXDIN      : in  std_logic_vector(7 downto 0);
+    RXKIN      : in  std_logic;
+    RXIO_P     : out std_logic;
+    RXIO_N     : out std_logic;
     DEBUGSTATE : out std_logic_vector(3 downto 0);
-    DECODEERR : out std_logic
+    DECODEERR  : out std_logic
     );
 
 end devicelink;
 
 architecture Behavioral of devicelink is
 
-  signal nottxlocked     : std_logic := '0';
-  signal dcmlocked       : std_logic := '0';
-  signal txclkint, txclk : std_logic := '0';
-  signal rxhbitclk       : std_logic := '0';
-  signal rxhbitclk180    : std_logic := '0';
-  signal rst             : std_logic := '0';
+  signal nottxlocked                   : std_logic := '0';
+  signal dcmlocked                     : std_logic := '0';
+  signal txclkint, txclk               : std_logic := '0';
+  signal rxhbitclk, rxhbitclkint       : std_logic := '0';
+  signal rxhbitclk180, rxhbitclk180int : std_logic := '0';
+  signal rst                           : std_logic := '0';
 
 
-  signal txdinl, txdinll  : std_logic_vector(9 downto 0) := (others => '0');
-  signal ltxdout : std_logic_vector(7 downto 0) := (others => '0');
+  signal txdinl, txdinll : std_logic_vector(9 downto 0) := (others => '0');
+  signal ltxdout         : std_logic_vector(7 downto 0) := (others => '0');
 
   signal cerr    : std_logic := '0';
   signal derr    : std_logic := '0';
@@ -54,19 +54,19 @@ architecture Behavioral of devicelink is
 
   signal ol, oll : std_logic_vector(9 downto 0) := (others => '0');
 
-  signal forceerr  : std_logic                    := '0';
-  signal txcodeerr : std_logic                    := '0';
+  signal forceerr     : std_logic                     := '0';
+  signal txcodeerr    : std_logic                     := '0';
   signal txcodeerrreg : std_logic_vector(63 downto 0) := (others => '1');
-  
-  signal sout      : std_logic_vector(9 downto 0) := (others => '0');
+
+  signal sout : std_logic_vector(9 downto 0) := (others => '0');
 
   signal rxio : std_logic := '0';
 
-  signal outbits : std_logic_vector(1 downto 0) := (others => '0');
-  signal ldebugstate : std_logic_vector(3 downto 0)  := (others => '0');
+  signal outbits     : std_logic_vector(1 downto 0) := (others => '0');
+  signal ldebugstate : std_logic_vector(3 downto 0) := (others => '0');
 
-  
-  type states is (none, sendsync,  lock, unlocked);
+
+  type states is (none, sendsync, lock, unlocked);
   signal cs, ns : states := none;
 
   component encode8b10b
@@ -115,7 +115,7 @@ begin  -- Behavioral
     CLKIN_PERIOD       => 20.0,
     CLKFX_DIVIDE       => 1,
     CLKFX_MULTIPLY     => 5,
-    DLL_FREQUENCY_MODE => "HIGH", 
+    DLL_FREQUENCY_MODE => "HIGH",
     DFS_FREQUENCY_MODE => "HIGH")
     port map (
       CLKIN            => TXCLKIN,
@@ -124,8 +124,8 @@ begin  -- Behavioral
       PSEN             => '0',
       CLK0             => txclkint,
       CLK2x            => CLK2X,
-      CLKFX            => rxhbitclk,
-      CLKFX180         => rxhbitclk180,
+      CLKFX            => rxhbitclkint,
+      CLKFX180         => rxhbitclk180int,
       LOCKED           => dcmlocked);
 
   txclk_bufg : BUFG port map (
@@ -134,16 +134,24 @@ begin  -- Behavioral
 
   CLK <= txclk;
 
+  rxhbitclk_bufg : BUFG port map (
+    O => rxhbitclk,
+    I => rxhbitclkint);
 
- DIN       <= rxdinl when dsel = '0' else X"1C";
- KIN       <= rxkinl when dsel = '0' else '1';
+  rxhbitclk180_bufg : BUFG port map (
+    O => rxhbitclk180,
+    I => rxhbitclk180int);
 
-  
+
+  DIN <= rxdinl when dsel = '0' else X"1C";
+  KIN <= rxkinl when dsel = '0' else '1';
+
+
   txcodeerr <= cerr or derr;
 
-  DECODEERR <= txcodeerr; 
+  DECODEERR  <= txcodeerr;
   DEBUGSTATE <= ldebugstate;
-  
+
   FDDRRSE_inst : FDDRRSE
     port map (
       Q  => rxio,                       -- Data output 
@@ -169,12 +177,12 @@ begin  -- Behavioral
   main : process (txclk, rst)
   begin  -- process main
     if rst = '1' then                   -- asynchronous reset
-      
-      cs   <= none;
+
+      cs           <= none;
       txcodeerrreg <= (others => '1');
 
-      
-                
+
+
     else
       if rising_edge(txclk) then
         cs <= ns;
@@ -182,10 +190,10 @@ begin  -- Behavioral
         rxdinl <= RXDIN;
         rxkinl <= RXKIN;
 
-        txdinl <= TXDIN;
-        txdinll <= txdinl ; 
-        TXDOUT <= ltxdout;
-        TXKOUT <= ltxkout;
+        txdinl  <= TXDIN;
+        txdinll <= txdinl;
+        TXDOUT  <= ltxdout;
+        TXKOUT  <= ltxkout;
 
         if forceerr = '0' then
           oll <= ol;
@@ -210,7 +218,7 @@ begin  -- Behavioral
       else
         sout <= "00" & sout(9 downto 2);
       end if;
-      
+
     end if;
 
   end process rxclkproc;
@@ -219,34 +227,34 @@ begin  -- Behavioral
   begin
     case cs is
       when none     =>
-        dsel     <= '1';
-        forceerr <= '0';
-        ldebugstate <= "0000"; 
-        ns       <= sendsync;
+        dsel        <= '1';
+        forceerr    <= '0';
+        ldebugstate <= "0000";
+        ns          <= sendsync;
       when sendsync =>
-        dsel     <= '1';
-        forceerr <= '0';
-        ldebugstate <= "0001"; 
+        dsel        <= '1';
+        forceerr    <= '0';
+        ldebugstate <= "0001";
         if ltxkout = '1' and ltxdout = X"FE" and txcodeerrreg = X"0000000000000000" then
-          ns     <= lock;
+          ns        <= lock;
 
         else
-          ns     <= sendsync;
+          ns        <= sendsync;
         end if;
       when lock     =>
-        dsel     <= '0';
-        forceerr <= '0';
-        ldebugstate <= "0011";         
+        dsel        <= '0';
+        forceerr    <= '0';
+        ldebugstate <= "0011";
         if txcodeerr = '1' then
-          ns     <= unlocked;
+          ns        <= unlocked;
         else
-          ns     <= lock;
+          ns        <= lock;
         end if;
       when unlocked =>
-        dsel     <= '0';
-        forceerr <= '1';
-        ldebugstate <= "0100"; 
-        ns       <= none;
+        dsel        <= '0';
+        forceerr    <= '1';
+        ldebugstate <= "0100";
+        ns          <= none;
       when others   => null;
     end case;
 
