b9ddf236c01081bf855d749edb33bd471064fbe0
/C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Eric M Jonas/emailAddress=jonas@MIT.EDU </C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Eric M Jonas/emailAddress=jonas@MIT.EDU@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2008-02-20 17:44:56 +0000

    reply from writebytes, correct this time
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/backplane/trunk@2663 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- tgt/backplane/vhdl/backplane_mem.vhd
+++ tgt/backplane/vhdl/backplane_mem.vhd
@@ -19,7 +19,7 @@ package backplane_mem_pkg is
 		constant syscontrol_inst_instruction_ram_INIT_0F : bit_vector(0 to 255) := X"10494AA0A07910394570A07910194A40A07910094DE0A07910790F2010878809";
 		constant syscontrol_inst_instruction_ram_INIT_10 : bit_vector(0 to 255) := X"88290009881A001A10B04E20A07910894D60A07910694CD0A07910594B40A079";
 		constant syscontrol_inst_instruction_ram_INIT_11 : bit_vector(0 to 255) := X"10170B3111D080890B3980990A291180880B120B887B091B8849032988390029";
-		constant syscontrol_inst_instruction_ram_INIT_12 : bit_vector(0 to 255) := X"0C420B3312D088728879091980C080A90D5980A90C4980A90B3980A90A291200";
+		constant syscontrol_inst_instruction_ram_INIT_12 : bit_vector(0 to 255) := X"0C420B3312D088028879091980C080A90D5980A90C4980A90B3980A90A291200";
 		constant syscontrol_inst_instruction_ram_INIT_13 : bit_vector(0 to 255) := X"13F01057106880C080A90E6980A90D5980A90C4980A90B39133080B913101047";
 		constant syscontrol_inst_instruction_ram_INIT_14 : bit_vector(0 to 255) := X"0000000000000000000000000000000000000000000000000000000000000000";
 		constant syscontrol_inst_instruction_ram_INIT_15 : bit_vector(0 to 255) := X"0000000000000000000000000000000000000000000000000000000000000000";
