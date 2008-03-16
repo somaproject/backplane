576da6fb2ba9b3d703eacf7cbb4a2c9c36b759db
/C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Eric M Jonas/emailAddress=jonas@MIT.EDU </C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Eric M Jonas/emailAddress=jonas@MIT.EDU@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2008-03-16 16:06:18 +0000

    Changed txclkdcm DLL_FREQUENCY_MODE to HIGH to reduce the complications of dll-reset
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/backplane/trunk@2735 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- deviceio/vhdl/devicelink.vhd
+++ deviceio/vhdl/devicelink.vhd
@@ -115,7 +115,7 @@ begin  -- Behavioral
     CLKIN_PERIOD       => 20.0,
     CLKFX_DIVIDE       => 1,
     CLKFX_MULTIPLY     => 5,
-    DLL_FREQUENCY_MODE => "HIGH",
+    DLL_FREQUENCY_MODE => "LOW",
     DFS_FREQUENCY_MODE => "HIGH")
     port map (
       CLKIN            => TXCLKIN,
