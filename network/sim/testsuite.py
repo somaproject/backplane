00a471e0eae8dc15b0712b5e98b8039870f09dcd
Eric Jonas <jonas@mit.edu>
2009-02-07 21:00:39 -0500

    * datapacketgen works

--- network/sim/testsuite.py
+++ network/sim/testsuite.py
@@ -23,7 +23,7 @@ else:
     suite.addTest(vhdlTestCase("inputcontrol"))
     suite.addTest(vhdlTestCase("pingresponse"))
     suite.addTest(vhdlTestCase("eventtx"))
-##     suite.addTest(vhdlTestCase("datapacketgen"))
+    suite.addTest(vhdlTestCase("datapacketgen"))
 ##     suite.addTest(vhdlTestCase("eventrx"))
 ##     #suite.addTest(vhdlTestCase("eventretx"))
 ##     suite.addTest(vhdlTestCase("data"))
