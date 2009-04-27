b730d7bc58eb6f0f96c905f1e93398792e76dc7a
Eric Jonas <jonas@mit.edu>
2009-04-27 19:07:08 -0400

    devicelinkstatus now shows the number of link-dropped events; manual boot correctly masks

--- tgt/backplane/devicelinkstatus.py
+++ tgt/backplane/devicelinkstatus.py
@@ -29,7 +29,32 @@ ea[eaddr.SYSCONTROL] = 1
 eio.sendEvent(ea, e)
 
 erx = eio.getEvents()
-print erx[0]
+linkstatus_event = erx[0]
 
+# now get all of the counter events
+
+rx_set = {}
+for i in range(20):
+    e = Event()
+    e.src = src
+    e.cmd = 0x21
+    e.data[0] = i
+
+    ea = eaddr.TXDest()
+    ea[eaddr.SYSCONTROL] = 1
+    eio.sendEvent(ea, e)
+
+    erx = eio.getEvents()
+    rx_set[i] = erx
+
+    
 eio.stop()
+for i in range(20):
+    if (linkstatus_event.data[1] >> i) & 0x1 > 0:
+        print "Device %2d : UP" % i, 
+    else:
+        print "Device %2d :   " % i,
+    print "%d link cycles"  % rx_set[i][0].data[2]
+
+
 
