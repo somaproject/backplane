32f406d3b1c0cfcf33f8d8031d67b399d4f78b92
Eric Jonas <jonas@mit.edu>
2009-11-22 13:38:26 -0500

    Updated boot sequencer to wait for devicelinks up removed duplicate manual_boot_dsp created stand-alone 'youare' script

--- tgt/backplane/manual_boot_dsp.py
+++ tgt/backplane/manual_boot_dsp.py
@@ -2,7 +2,9 @@
 
 Manually boot some subset of the DSP FPGAs with the indicated bitfile
 
-manual-boot-dsp.py dspnum bitfile.bit
+manual-boot-dsp.py bitfile.bit dspnum
+
+Now also waits for devicelink status to be UP
 
 """
 import sys
@@ -13,6 +15,16 @@ from somapynet.neteventio import NetEventIO
 import struct
 import time
 
+# This is an awful hack: right now our config
+# line settings are 0-19, but we have remapped
+# the physical locations (pins) of the other devices
+# on the board. So this is how we get around it.
+
+device_to_config_mapping = { 0 : 0,
+                             1 : 1,
+                             2 : 6,
+                             3 : 7}
+
 def reallysend(eio, ea, ed):
     while True:
         try:
@@ -22,6 +34,10 @@ def reallysend(eio, ea, ed):
             print "Whoa, we didn't hear the answer" 
             pass
 
+
+def parse_linkstatus(e):
+    return [(e.data[1] >> i) & 0x1 > 0 for i in range(16)]
+    
 def manual_boot_dsp(filename, devicenums):
 
     eio = NetEventIO("10.0.0.2")
@@ -36,14 +52,16 @@ def manual_boot_dsp(filename, devicenums):
     MANBOOTSER_SENDBYTES = 0xA3
 
     EVENTCMD_YOUARE = 0x01
-
+    EVENTCMD_LINKSTATUS = 0x20
+    
 
     e = Event()
     e.src = eaddr.NETWORK
     e.cmd =  MANBOOTSER_SETMASK
 
     wrd = 0
-    for d in [int(x) for x in devicenums]:
+    int_devicenums = [int(x) for x in devicenums]
+    for d in [device_to_config_mapping[x] for x in int_devicenums]:
         wrd |= (1 << (d + 4))
     #print "word = %8.8X" % wrd
 
@@ -99,22 +117,61 @@ def manual_boot_dsp(filename, devicenums):
 
         ecnt  += 1
 
+    # loop and query the linkstatus register to see if our links
+    # are up yet, and once they are, go.
 
-    time.sleep(1) # superfluous sleep to allow for boot up
+    up_delay = False
+    allup = False
+    for i in range(20):
+        e = Event()
+        e.cmd = EVENTCMD_LINKSTATUS
+        e.src = eaddr.NETWORK
+        ea = eaddr.TXDest()
+        ea[eaddr.SYSCONTROL] = 1
 
+        reallysend(eio, ea, e)
+
+        erx =  eio.getEvents(False)
+        device_status_event = None
+        
+        if erx != None:
+            for e in erx:
+                if e.cmd == EVENTCMD_LINKSTATUS:
+                    device_status_event = e
+
+        if device_status_event:
+            stat = parse_linkstatus(device_status_event)
+            allup = True
+            for i in int_devicenums:
+                if not stat[i]:
+                    allup = False
+        if allup:
+            break
+        up_delay += 1
+        time.sleep(1)
+        
+    if not allup:
+        print "Was unable to bring up the requested links"
+        sys.exit(1)
+    else:
+        print "links up after", up_delay, "secs"
+    
     #now send the YOUARE so the device knows who it is
     for i in devicenums:
         devicelinknum = int(i)
 
         for device in xrange(4):
+             e = Event()
              e.cmd = EVENTCMD_YOUARE
              e.src = eaddr.NETWORK
              deviceid = 8  + devicelinknum * 4 + device
-             #print "Sending youare for ", deviceid
+             
              e.data[0] = deviceid
              ea = eaddr.TXDest()
              ea[deviceid] = 1
-             eio.sendEvent(ea, e)
+             print "Sending youare for ", deviceid, e
+             
+             reallysend(eio, ea, e)                        
 
 
     eio.stop()
